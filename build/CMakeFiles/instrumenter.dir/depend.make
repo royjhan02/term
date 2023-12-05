c0a0721e380b14a56f18a50eae0c638a360dc764
Kumar Madhukar <madhukar@macpro.local>
2023-12-05 10:13:38 +0530

    updates

--- build/CMakeFiles/instrumenter.dir/depend.make
+++
@@ -1,2 +0,0 @@
-# Empty dependencies file for instrumenter.
-# This may be replaced when dependencies are built.
--- common.h
+++
@@ -1,8 +0,0 @@
-extern clang::SourceLocation mainEndLoc;
-extern std::string missingFunctions;
-extern std::string callExprType;
-extern std::string callExprName;
-
-extern int instrumentation_flag;
-
-#define DEBUG_INST
